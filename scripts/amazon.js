e220d804b15ed87f22e1337528fb8687cbb33b8e
aditya <adityagopinath2112@gmail.com>
2024-08-07 18:17:52 +0400

    added the added animation when button clicked

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -40,7 +40,7 @@ products.forEach((product) =>{
 
           <div class="product-spacer"></div>
 
-          <div class="added-to-cart">
+          <div class="added-to-cart js-added-to-cart-${product.id}">
             <img src="images/icons/checkmark.png">
             Added
           </div>
@@ -72,6 +72,13 @@ document.querySelectorAll('.js-add-to-cart')
 
             const quantitySelector = document.querySelector(`.js-quantity-selector-${productId}`);
 
+            const addedMessage = document.querySelector(`.js-added-to-cart-${productId}`);
+
+            addedMessage.classList.add('added-to-cart-visible');
+
+            setTimeout(() =>{
+              addedMessage.classList.remove('added-to-cart-visible');
+            },2000);
 
             const quantity = Number(quantitySelector.value);
 

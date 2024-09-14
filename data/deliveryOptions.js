ede2676a166fe0c1a85c28409573fab47625a5d6
aditya <adityagopinath2112@gmail.com>
2024-09-14 15:59:30 +0400

    added tests and new money function

--- data/deliveryOptions.js
+++ data/deliveryOptions.js
@@ -25,12 +25,24 @@ export function getDeliveryOption(deliveryOptionId){
 
     return deliveryOption || deliveryOption[0];
 }
+function isWeekend(date){
+    const dayofweek = date.format('dddd');
+    return dayofweek === 'Saturday' || dayofweek === 'Sunday';
+}
+
 export function calculateDeliveryDate(deliveryOption) {
-    const today = dayjs();
-    const deliveryDate = today.add(
-      deliveryOption.deliveryDays,
-      'days'
-    );
+    let remainingDays = deliveryOption.deliveryDays;
+    let deliveryDate = dayjs();
+
+    while (remainingDays > 0) {
+        deliveryDate = deliveryDate.add(1, 'day');
+
+        if (!isWeekend(deliveryDate)) {
+            remainingDays--;
+            // This is a shortcut for:
+            // remainingDays = remainingDays - 1;
+        }
+    }
     const dateString = deliveryDate.format(
       'dddd, MMMM D'
     );
